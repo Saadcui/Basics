5e1c1c0f187b1d46b2a005506bcf2ce0d844fca0
Saadcui <saadinves04@gmail.com>
2025-08-22 18:24:12 +0500

    update

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -106,6 +105,4 @@ app.delete("/passwords/:id", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+module.exports = app;
