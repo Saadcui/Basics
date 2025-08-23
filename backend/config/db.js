cc52fa4c5ce32a45b37e056a0cf644aeddbe11f8
Saadcui <saadinves04@gmail.com>
2025-08-23 17:06:34 +0500

    updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,27 @@
 import mongoose from "mongoose";
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+    }).then((mongoose) => {
+      return mongoose;
     });
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.error(" MongoDB connection failed", error.message);
-    process.exit(1);
   }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
 export default connectDB;
